8113b073c9ecd4dc6585ca9fc19e6ab0e982903a
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-06-04 17:26:41 -0600

    Committed old changes before restructuring project

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,3 +1,5 @@
+import scraper.apis
+import scraper.parsing
 
 from ._version import get_versions
 __version__ = get_versions()['version']
--- scraper/reference_scraper.py
+++ scraper/reference_scraper.py
@@ -1,12 +1,5 @@
 #!/bin/env python3
-import requests, bs4, re, sys, doi
-
-def valid_url(url):
-    try:
-        ping = requests.head(url)
-        return ping.status_code < 400
-    except Exception:
-        return False
+import sys
 
 args = sys.argv
 try:
@@ -14,21 +7,8 @@ try:
 except IndexError:
     raise ValueError("No URL was passed")
 outfile = args[2] if len(args) > 2 else None
-if not valid_url(url):
-    raise ValueError("The passed URL was improperly formatted")
-else:
-    scrape_url_for_refs(url, outfile=outfile)
-
-scrape_url_for_refs('https://en.wikipedia.org/wiki/Orchestrated_objective_reduction', outfile='/tmp/scraper')
-def scrape_url_for_refs(url, outfile=None):
-    page = requests.get(url)
-    parsed_page = bs4.BeautifulSoup(page.content)
-    links = parsed_page.findAll('a', attrs={'href': re.compile('')})
-    if outfile is not None:
-        with open(outfile, "w+") as fout:
-            doc_links = [href for link in links
-                         if 'arxiv' in (href := link.get('href'))
-                         or 'doi' in href]
-            lines = ['source_url:{}\n'.format(url), *map(lambda l: l + '\n', doc_links)]
-            fout.writelines(lines)
+# if not valid_url(url):
+    # raise ValueError("The passed URL was improperly formatted")
+# else:
+    # scrape_url_for_refs(url, outfile=outfile)
 
