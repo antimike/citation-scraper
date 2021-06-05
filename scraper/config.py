c349c00e87c303787705f479ec15563ff5f77b18
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-06-04 20:35:19 -0600

    Added 'Loader' kwarg to yaml.load to avoid error message

--- scraper/config.py
+++ scraper/config.py
@@ -2,4 +2,4 @@ from pathlib import Path
 import yaml
 
 with open("{}/config/wiki_config.yaml".format(Path(__file__).parent.parent), 'r') as f:
-    config = yaml.load(f)
+    config = yaml.load(f, Loader=yaml.FullLoader)
