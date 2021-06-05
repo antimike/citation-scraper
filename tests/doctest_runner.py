354e90677e4ea3a080cf3f1313838774d3742e37
Michael Haynes <Antimike.Haynes@colostate.edu>
2021-06-04 20:37:10 -0600

    Moved test dir

--- tests/doctest_runner.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-sys.path.append("{}/scraper".format(os.environ["CITATION_SCRAPER_ROOT"]))
-import scraper
-
-if __name__ == "main":
-    import doctest
-    # doctest.test_mod(scraper.parsing)
